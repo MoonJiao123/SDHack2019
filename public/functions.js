e9ca96cbc94c909a51919214161227a87ce3ff5d
duouyang <duouyang@ucsd.edu>
2019-10-27 02:40:11 -0700

    generator

--- public/functions.js
+++ public/functions.js
@@ -17,11 +17,16 @@ function select() {
        },
        success: function(data) {
             //for each loop
-            for(recipe in data){
+            // console.log(data[0].name);
+            for (var i = 0; i<data.length; i++){
+                var recipe = data[i];
+                // console.log(recipe.name);
+                $(".dymhtml").empty();
+                // console.log(recipe);
                 var name = recipe.name;
                 var url = recipe.url;
                 var image = recipe.image;
-                html = " <div class=\"col-lg-4 col-md-6 col-sm-12 icon-slide-container addPadding\"> <a href="
+                html = " <div class=\"col-lg-4 col-md-6 col-sm-12 icon-slide-container addPadding\"> <a href=\""
                 html += url+"\">";
                 html +=  " <div class=";
                 html += "<div class=\"image-container image\" style=\"background-image:url(";
