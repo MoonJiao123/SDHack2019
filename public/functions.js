2974a9462560ed183cb62a296d386cec1e27d018
MoonJiao123 <yuj001@ucsd.edu>
2019-10-27 02:05:26 -0700

    heml

--- public/functions.js
+++ public/functions.js
@@ -16,7 +16,22 @@ function select() {
             ingredient : ingredients
        },
        success: function(data) {
-            console.log(data);
+            //for each loop
+            for(recipe in data){
+                var name = recipe.name;
+                var url = recipe.url;
+                var image = recipe.image;
+                html = " <div class=\"col-lg-4 col-md-6 col-sm-12 icon-slide-container addPadding\"> <a href="
+                html += url+"\">";
+                html +=  " <div class=";
+                html += "<div class=\"image-container image\" style=\"background-image:url(";
+                html += image;
+                html += ")></div><h4>";
+                html += name;
+                html += "</h4></a></div>";
+                $(html).insertAfter(".dymhtml");
+                console.log(html);      
+            }
        },
        error: function(err) {
             console.log(err);
