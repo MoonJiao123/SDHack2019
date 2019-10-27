96c5f2d24ee5f6a8ce933a396c97799a2e9d7062
Mielan <Mielan@users.noreply.github.com>
2019-10-27 05:10:39 -0700

    a

--- public/functions.js
+++ public/functions.js
@@ -2,49 +2,50 @@
 function select() {
     var ingredients = []
     var boxes = document.getElementsByName("ingredient-check");
-    for (var i = 0; i<boxes.length; i++) {
-        if(boxes[i].checked) {
+    for (var i = 0; i < boxes.length; i++) {
+        if (boxes[i].checked) {
             ingredients.push(boxes[i].value);
         }
     }
     console.log(ingredients);
 
     $.ajax({
-       type: 'post',
-       url: '/generate',
-       data : {
-            ingredient : ingredients
-       },
-       success: function(data) {
+        type: 'post',
+        url: '/generate',
+        data: {
+            ingredient: ingredients
+        },
+        success: function(data) {
             //for each loop
             // console.log(data[0].name);
             $("#dymhtml").html("");
-            for (var i = 0; i<data.length && i<9; i++){
+            for (var i = 0; i < data.length && i < 9; i++) {
                 var recipe = data[i];
                 console.log(recipe);
                 // console.log(recipe);
                 var name = recipe.name;
                 var url = recipe.url;
                 var image = recipe.image;
-                if(image == "https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public?itok=Y6-bwmRb"){
+                if (image == "https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public?itok=Y6-bwmRb") {
                     image = "./images/egg.jpg";
                 }
                 let html = " <div class=\"col-lg-4 col-md-6 col-sm-12 icon-slide-container addPadding\"> <a href=\"";
-                html += url+"\">";
+                html += url + "\" target='_blank'>";
                 html += "<div class=\"image-container image\" style=\"background-image:url('";
                 html += image;
-                html += "')\"></div><h4>";
+                html += "')\"></div><h4 class=\"limitLine\">";
                 html += name;
                 html += "</h4></a></div>";
-                $(html).insertAfter($("#dymhtml"));
-                console.log(html);      
+                $(html).hide().insertAfter($("#dymhtml")).fadeIn(500);
+                console.log(html);
             }
-       },
-       error: function(err) {
-            console.log(err);
-       }
 
-   });
+        },
+        error: function(err) {
+            console.log(err);
+        }
+
+    });
 }
 
 function insertImageSubmit() {
@@ -60,9 +61,9 @@ function insertImageSubmit() {
         contentType: false,
         processData: false,
         type: 'POST',
-        success: function(data){
+        success: function(data) {
             console.log(data);
         }
     });
-    
+
 }
