429406b98b60d381992917d64e9ca8626a1c1bb5
MoonJiao123 <yuj001@ucsd.edu>
2019-10-27 03:06:40 -0700

    font

--- public/functions.js
+++ public/functions.js
@@ -18,23 +18,25 @@ function select() {
        success: function(data) {
             //for each loop
             // console.log(data[0].name);
-            for (var i = 0; i<data.length; i++){
+            $("#dymhtml").html("");
+            for (var i = 0; i<data.length && i<9; i++){
                 var recipe = data[i];
-                // console.log(recipe.name);
-                $(".dymhtml").empty();
+                console.log(recipe);
                 // console.log(recipe);
                 var name = recipe.name;
                 var url = recipe.url;
                 var image = recipe.image;
-                html = " <div class=\"col-lg-4 col-md-6 col-sm-12 icon-slide-container addPadding\"> <a href=\""
+                if(image == "https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public?itok=Y6-bwmRb"){
+                    image = "./images/egg.jpg";
+                }
+                let html = " <div class=\"col-lg-4 col-md-6 col-sm-12 icon-slide-container addPadding\"> <a href=\"";
                 html += url+"\">";
-                html +=  " <div class=";
-                html += "<div class=\"image-container image\" style=\"background-image:url(";
+                html += "<div class=\"image-container image\" style=\"background-image:url('";
                 html += image;
-                html += ")></div><h4>";
+                html += "')\"></div><h4>";
                 html += name;
                 html += "</h4></a></div>";
-                $(html).insertAfter(".dymhtml");
+                $(html).insertAfter($("#dymhtml"));
                 console.log(html);      
             }
        },
